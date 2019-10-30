cbe53509a025aa0a76a6e186ad420e3dffe0a05c
narpatramv <57184496+narpatramv@users.noreply.github.com>
2019-10-30 21:56:26 +0530

    Update file.c

--- file.c
+++ file.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-int i,sum=0;
+int i,sum=0;//initialize the sum
 for(i=1;i<=10;i++)
 {
-if(i%2==0)
+if(i%2==0)//condition for even no
 {
 printf("i=%d\n",i);
 sum=sum+i;
